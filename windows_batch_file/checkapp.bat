2a5772808bff5fec505639223a2d0ec39b6ae3f9
Daoxue Wu <jason36334720@yahoo.com.tw>
2022-03-07 08:38:39 +0800

    為 checkapp.bat 添加一些註解

--- windows_batch_file/checkapp.bat
+++ windows_batch_file/checkapp.bat
@@ -1,9 +1,11 @@
 @echo off
+::tasklist /fi "<篩選器> <運算子> <值>"
 TASKKILL /F /IM APP_NAME.exe /FI "STATUS eq NOT RESPONDING"
 tasklist /FI "IMAGENAME eq APP_NAME.exe" 2>NUL | find /I /N "APP_NAME.exe" >NUL
 if "%ERRORLEVEL%"=="0" (
 exit 
 ) else (
+::PAUSE 會暫停，cmd 視窗就會停住，等你按任意鍵繼續
 PUSHD
 cd /d "C:\your APP_NAME.exe path\"
 start APP_NAME.exe
