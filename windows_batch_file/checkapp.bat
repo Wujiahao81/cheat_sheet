799deef40a0ba43db72bb74583ee6d0eb6567820
Daoxue Wu <jason36334720@yahoo.com.tw>
2022-03-07 09:25:03 +0800

    添加註解

--- windows_batch_file/checkapp.bat
+++ windows_batch_file/checkapp.bat
@@ -1,6 +1,7 @@
 @echo off
 ::tasklist /fi "<篩選器> <運算子> <值>"
 TASKKILL /F /IM APP_NAME.exe /FI "STATUS eq NOT RESPONDING"
+::taskkill /im <映像名稱>
 tasklist /FI "IMAGENAME eq APP_NAME.exe" 2>NUL | find /I /N "APP_NAME.exe" >NUL
 if "%ERRORLEVEL%"=="0" (
 exit 
